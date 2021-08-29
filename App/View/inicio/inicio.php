0eadcfef0d20d30f7ecf30ecfbe4225833eacae0
GTIT\ramon.nascimento <ramon.nascimento@guiandotelecom.com.br>
2021-08-29 20:46:44 -0300

    ajuste visual tela inicial, com responsividade

--- App/View/inicio/inicio.php
+++ App/View/inicio/inicio.php
@@ -4,65 +4,58 @@ require "../../Controller/LoginController.php";
 require "../../Structure/importsPagesStyle.php";
 require "../../Structure/header.php";
 ?>
+<link rel="stylesheet" href="inicio.css">
 <br>
-<center>
-    <div class="container">
-        <h1>Central Motos NB</h1>
-    </div>
+<div class="container col-12">
+    <h1>Central Motos NB</h1>
+
     <p>
         Gestão de entregas rápidas Central Motos NB
     </p>
-    <br>
-    <div class="container">
-        <div class="row">
-            <div class="col-3 px-1">
-                <div class="card" style="width: 18rem; height: 20rem;">
-                    <img class="card-img-top" src="../../img/1.jpg" alt="Card image cap"
-                        style="height: 155px; width: 285px;">
-                    <div class="card-body">
-                        <h5 class="card-title">Pedidos</h5>
-                        <p class="card-text">Gestão de pedidos.</p>
-                        <a href="../pedido/listar.php" class="btn btn-primary">Acessar</a>
-                    </div>
+
+    <div class="row">
+        <div class="col-12 col-md-3 col-lg-3 col-xl-3 card_item">
+            <div class="card">
+                <div class="card-header card_header_person1"></div>
+                <div class="card-body">
+                    <h5 class="card-title">Pedidos</h5>
+                    <p class="card-text">Gestão de pedidos.</p>
+                    <a href="../pedido/listar.php" class="btn btn-primary">Acessar</a>
                 </div>
             </div>
-            <div class="dropdown-divider"></div>
-            <div class="col-3 px-1">
-                <div class="card" style="width: 18rem; height:20rem">
-                    <img class="card-img-top" src="../../img/3.jpg" alt="Card image cap"
-                        tyle="height: 200px; width: 280px;">
-                    <div class="card-body">
-                        <h5 class="card-title">Usuarios</h5>
-                        <p class="card-text">Gestão de usuários do sistema.</p>
-                        <a href="../usuario/listar.php" class="btn btn-primary">Acessar</a>
-                    </div>
+        </div>
+        <div class="col-12 col-md-3 col-lg-3 col-xl-3 card_item">
+            <div class="card">
+                <div class="card-header card_header_person2"></div>
+                <div class="card-body">
+                    <h5 class="card-title">Bairros</h5>
+                    <p class="card-text">Gestão de Bairros.</p>
+                    <a href="../bairro/listar.php" class="btn btn-primary">Acessar</a>
                 </div>
             </div>
-            <div class="col-3 px-1">
-                <div class="card" style="width: 18rem; height:20rem">
-                    <img class="card-img-top" src="../../img/2.jpg" alt="Card image cap"
-                        style="height: 150px; width: 280px;">
-                    <div class="card-body">
-                        <h5 class="card-title">Bairros</h5>
-                        <p class="card-text">Gestão de Bairros.</p>
-                        <a href="../bairro/listar.php" class="btn btn-primary">Acessar</a>
-                    </div>
+        </div>
+        <div class="col-12 col-md-3 col-lg-3 col-xl-3 card_item">
+            <div class="card">
+                <div class="card-header card_header_person3"></div>
+                <div class="card-body">
+                    <h5 class="card-title">Usuarios</h5>
+                    <p class="card-text">Gestão de usuários do sistema.</p>
+                    <a href="../usuario/listar.php" class="btn btn-primary">Acessar</a>
                 </div>
             </div>
-            <div class="col-3 px-1">
-                <div class="card" style="width: 18rem; height: 20rem;">
-                    <img class="card-img-top" src="../../img/1.jpg" alt="Card image cap"
-                        style="height: 155px; width: 285px;">
-                    <div class="card-body">
-                        <h5 class="card-title">Relatórios</h5>
-                        <p class="card-text">Relatórios de Pedidos.</p>
-                        <a href="../relatorio/pedidos.php" class="btn btn-primary">Acessar</a>
-                    </div>
+        </div>
+        <div class="col-12 col-md-3 col-lg-3 col-xl-3 card_item">
+            <div class="card">
+                <div class="card-header card_header_person4"></div>
+                <div class="card-body">
+                    <h5 class="card-title">Relatórios</h5>
+                    <p class="card-text">Relatórios de Pedidos.</p>
+                    <a href="../relatorio/pedidos.php" class="btn btn-primary">Acessar</a>
                 </div>
             </div>
         </div>
     </div>
-</center>
+</div>
 <?php
 require "../../Structure/importsPagesJs.php";
 require "../../Structure/footer.php";
